f9e6a6521c6c2108667caca73c716e2a03969f38
carlosmab <carlosm.araujob@gmail.com>
2023-01-30 10:59:03 -0500

    Configure test environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,15 @@
-from fastapi.testclient import TestClient
-
 import pytest
+from fastapi.testclient import TestClient
 
 from src import main
 
+@pytest.fixture(scope="session")
+def app():
+    """Fixture to get FastAPI app"""
+    
+    return main.app
+
+@pytest.fixture(scope="session")
+def client(app):
+    """Fixture to get FastAPI client"""
+    return TestClient(app)
